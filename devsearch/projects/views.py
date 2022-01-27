da9ef14e5ae90da7ffa7d269d4de8243e1b4951d
ojashshrestha1 <ojashshrestha1@gmail.com>
2022-01-27 22:18:26 +0545

    template html page loaded

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse('Here are our products')
+    return render(request, 'projects.html')
 
 def project(request, pk):
     return HttpResponse('SINGLE PROJECT' + ' ' + str(pk) )
