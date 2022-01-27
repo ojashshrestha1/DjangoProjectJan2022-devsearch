a5e2a8758097738ba49f360eab6a2586eb9f4d03
ojashshrestha1 <ojashshrestha1@gmail.com>
2022-01-27 23:01:02 +0545

    Message Pass - Variable pass from view.py to html template

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    msg = 'Hello, you are on the projects page'
+    return render(request, 'projects/projects.html', {'message':msg})
 
 # def project(request, pk):
 #     return HttpResponse('SINGLE PROJECT' + ' ' + str(pk) )
