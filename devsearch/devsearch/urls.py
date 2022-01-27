ca1623ea1ec669dfdf624d7a2823297c658da940
ojashshrestha1 <ojashshrestha1@gmail.com>
2022-01-27 17:07:36 +0545

    Dynamic Url str pk

--- devsearch/devsearch/urls.py
+++ devsearch/devsearch/urls.py
@@ -20,12 +20,12 @@ from django.http import HttpResponse
 def projects(request):
     return HttpResponse('Here are our products')
 
-def project(request):
-    return HttpResponse('SINGLE PROJECT')
+def project(request, pk):
+    return HttpResponse('SINGLE PROJECT' + ' ' + str(pk) )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('home/', getHomePage),
     path('projects/', projects, name="projects"),
-    path('project/', project, name="projects"),
+    path('project/<str:pk>/', project, name="projects"),
 ]
