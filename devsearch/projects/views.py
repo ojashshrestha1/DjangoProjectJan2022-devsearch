5987af9d1d91d1c48e02b2461af24221da386744
ojashshrestha1 <ojashshrestha1@gmail.com>
2022-01-27 22:25:07 +0545

    single project html page load

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -6,5 +6,8 @@ from django.http import HttpResponse
 def projects(request):
     return render(request, 'projects.html')
 
+# def project(request, pk):
+#     return HttpResponse('SINGLE PROJECT' + ' ' + str(pk) )
+
 def project(request, pk):
-    return HttpResponse('SINGLE PROJECT' + ' ' + str(pk) )
+    return render(request, 'single-projects.html')
