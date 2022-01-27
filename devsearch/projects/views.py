9d59bfa49dbfe07b4dc6070d7171769054c53523
ojashshrestha1 <ojashshrestha1@gmail.com>
2022-01-27 23:47:39 +0545

    Dictionary list data show  from view to template html

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -2,12 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
 
 def projects(request):
     # msg = 'Hello, you are on the projects page'
     page = 'aloo'
     number = 10
-    context = {'pagex':page, 'number': number}
+    context = {'pagex':page, 'number': number,'projects':projectsList}
     # return render(request, 'projects/projects.html', {'pagex':page})
 
     return render(request, 'projects/projects.html', context)
