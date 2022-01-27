a6cadfecf0bdc2c468df188d1e5106b3bd653863
ojashshrestha1 <ojashshrestha1@gmail.com>
2022-01-27 22:05:30 +0545

    urls setup within projects app

--- devsearch/devsearch/urls.py
+++ devsearch/devsearch/urls.py
@@ -14,18 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-def projects(request):
-    return HttpResponse('Here are our products')
-
-def project(request, pk):
-    return HttpResponse('SINGLE PROJECT' + ' ' + str(pk) )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('home/', getHomePage),
-    path('projects/', projects, name="projects"),
-    path('project/<str:pk>/', project, name="projects"),
+    path( '', include('projects.urls'))
+   
 ]
