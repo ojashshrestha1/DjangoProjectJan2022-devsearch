eff696c1f53305c4068f528f171697e02e4abd6e
ojashshrestha1 <ojashshrestha1@gmail.com>
2022-01-27 23:30:43 +0545

    if else logic in template

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -4,11 +4,17 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    msg = 'Hello, you are on the projects page'
-    return render(request, 'projects/projects.html', {'message':msg})
+    # msg = 'Hello, you are on the projects page'
+    page = 'aloo'
+    number = 10
+    context = {'pagex':page, 'number': number}
+    # return render(request, 'projects/projects.html', {'pagex':page})
+
+    return render(request, 'projects/projects.html', context)
 
 # def project(request, pk):
 #     return HttpResponse('SINGLE PROJECT' + ' ' + str(pk) )
 
 def project(request, pk):
+    
     return render(request, 'projects/single-projects.html')
