c762f3b6759704f2f10fc22951de61188f175e11
ojashshrestha1 <ojashshrestha1@gmail.com>
2022-01-29 22:34:13 +0545

    __str__ self.name bug fixed for Tag and Review Classes in Model

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -33,7 +33,7 @@ class Review(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
-    def __str(self):
+    def __str__(self):
         return self.value
 
 
@@ -42,7 +42,7 @@ class Tag(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
-    def __str(self):
+    def __str__(self):
         return self.name
 
 
