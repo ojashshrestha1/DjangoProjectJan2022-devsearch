18a850d99dfecab32435fa90ff81a4baaa0dba69
ojashshrestha1 <ojashshrestha1@gmail.com>
2022-01-30 00:02:10 +0545

    Views Updated with Database data - Titles, Tags

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
+
+
 # Create your views here.
 
 projectsList = [
@@ -21,22 +24,33 @@ projectsList = [
 ]
 
 
-def projects(request):
-    # msg = 'Hello, you are on the projects page'
-    page = 'aloo'
-    number = 10
-    context = {'pagex':page, 'number': number,'projects':projectsList}
-    # return render(request, 'projects/projects.html', {'pagex':page})
+# def projects(request):
+#     # msg = 'Hello, you are on the projects page'
+#     page = 'aloo'
+#     number = 10
+#     context = {'pagex':page, 'number': number,'projects':projectsList}
+#     # return render(request, 'projects/projects.html', {'pagex':page})
 
+#     return render(request, 'projects/projects.html', context)
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
 # def project(request, pk):
 #     return HttpResponse('SINGLE PROJECT' + ' ' + str(pk) )
 
-def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i ['id'] == pk:
-            projectObj = i
+# def project(request, pk):
+#     projectObj = None
+#     for i in projectsList:
+#         if i ['id'] == pk:
+#             projectObj = i
 
-    return render(request, 'projects/single-projects.html', {'project': projectObj})
+#     return render(request, 'projects/single-projects.html', {'project': projectObj})
+
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    tags = projectObj.tags.all() 
+    print('projectObj:', projectObj)
+    return render(request, 'projects/single-projects.html', {'project': projectObj, 'tags': tags })
