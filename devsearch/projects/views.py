5e5c273a00d0d772de1894d9541e76ecad0bd8c0
ojashshrestha1 <ojashshrestha1@gmail.com>
2022-01-30 19:56:17 +0545

    project_form.html

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -5,23 +5,23 @@ from .models import Project
 
 # Create your views here.
 
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
+# projectsList = [
+#     {
+#         'id': '1',
+#         'title': 'Ecommerce Website',
+#         'description': 'Fully functional ecommerce website'
+#     },
+#     {
+#         'id': '2',
+#         'title': 'Portfolio Website',
+#         'description': 'A personal website to write articles and display work'
+#     },
+#     {
+#         'id': '3',
+#         'title': 'Social Network',
+#         'description': 'An open source project built by the community'
+#     }
+# ]
 
 
 # def projects(request):
@@ -33,10 +33,7 @@ projectsList = [
 
 #     return render(request, 'projects/projects.html', context)
 
-def projects(request):
-    projects = Project.objects.all()
-    context = {'projects': projects}
-    return render(request, 'projects/projects.html', context)
+
 
 # def project(request, pk):
 #     return HttpResponse('SINGLE PROJECT' + ' ' + str(pk) )
@@ -49,9 +46,18 @@ def projects(request):
 
 #     return render(request, 'projects/single-projects.html', {'project': projectObj})
 
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects/projects.html', context)
+
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     # tags = projectObj.tags.all() 
     print('projectObj:', projectObj)
     return render(request, 'projects/single-projects.html', {'project': projectObj })
     #  return render(request, 'projects/single-projects.html', {'project': projectObj, 'tags': tags })
+
+def createProject(request):
+    context = {}
+    return render(request, "projects/project_form.html", context)
