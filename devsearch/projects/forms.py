cc59178aa9bbbca51bddefd5a7ba79304321da26
ojashshrestha1 <ojashshrestha1@gmail.com>
2022-02-06 23:50:03 +0545

    fields update - forms.py

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -5,4 +5,4 @@ class ProjectForm(ModelForm):
     class Meta: 
         model = Project
         # fields = '__all__'
-        fields = ['title', 'description', 'demo_link']
+        fields = ['title', 'description', 'demo_link', 'source_link', 'tags']
