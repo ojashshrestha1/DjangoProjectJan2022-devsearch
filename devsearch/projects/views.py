a6077403ee5f0f48b45f67c649bbc67ba1a5d69f
ojashshrestha1 <ojashshrestha1@gmail.com>
2022-02-08 22:53:32 +0545

    bug resolved checking error details

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -76,7 +76,7 @@ def createProject(request):
 
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
-    form = ProjectForm(isntance=project)
+    form = ProjectForm(instance=project)
 
     if request.method == 'POST':
         # print(request.POST)
